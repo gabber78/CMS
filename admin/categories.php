366f8ab889e4452ed66d60fb24d64e2113e050d9
gabber78 <gabor.rab@gmail.com>
2020-02-05 12:10:55 +0100

    add/edit categories done, commit befor refactoring

--- admin/categories.php
+++ admin/categories.php
@@ -57,35 +57,12 @@
 
                         </form>
 
-
-                        <form action="" method="post">
-                            <div class="form-group">
-                                <label for="cat-title">Edit Category</label>
-                                <?php
-                                    
-                                    if (isset($_GET['edit'])) {
-                                        $cat_id = $_GET['edit'];
-
-                                        $query = "SELECT * FROM categories WHERE cat_id = $cat_id";
-                                        $select_categories_id = mysqli_query($connection, $query);
-
-                                        while ($row = mysqli_fetch_assoc($select_categories_id)) {
-                                            $cat_id = $row['cat_id'];
-                                            $cat_title = $row['cat_title'];
-                                         ?>
-
-                                         <input value="<?php if (isset($cat_title)){echo $cat_title;}?>" name="cat_title" type="text" class="form-control">
-
-                                        <?php } } ?>
-
-
-
-
-                            </div>
-                            <div class="form-group">
-                                <input class="btn btn-primary" name="submit" type="submit" value="Edit Category">
-                            </div>
-                        </form>
+                        <?php
+                            if (isset($_GET['edit'])){
+                                $cat_id = $_GET['edit'];
+                                include "includes/update_categories.php";
+                            }
+                        ?>
 
                 </div> <!-- Add + Edit category form-->
 
