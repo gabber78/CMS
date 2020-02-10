dce36ae8c156af264ed88a1f09a2fc0207a3c7aa
gabber78 <gabor.rab@gmail.com>
2020-02-10 19:55:36 +0100

    create posts section + add db queries

--- admin/functions.php
+++ admin/functions.php
@@ -1,5 +1,15 @@
 <?php
 
+function confirmQuery($result){
+
+    global $connection;
+
+    if (!$result) {
+        die("Query Failed" . mysqli_error($connection));
+    }
+}
+
+
 function insert_categories(){
 
     global $connection; //make variable $global accessible from anywhere
---
+++ admin/includes/view_all_posts.php
@@ -0,0 +1,63 @@
+<table class="table table-bordered table-hover">
+    <thead>
+    <tr>
+        <th>Id</th>
+        <th>Author</th>
+        <th>Title</th>
+        <th>Category</th>
+        <th>Status</th>
+        <th>Image</th>
+        <th>Tags</th>
+        <th>Comments</th>
+        <th>Date</th>
+    </tr>
+    </thead>
+
+    <tbody>
+    <?php
+    $query = "SELECT * FROM posts";
+    $select_posts = mysqli_query($connection, $query);
+
+    while ($row = mysqli_fetch_assoc($select_posts)) {
+        $post_id = $row['post_id'];
+        $post_author = $row['post_author'];
+        $post_title = $row['post_title'];
+        $post_category_id = $row['post_category_id'];
+        $post_status = $row['post_status'];
+        $post_image = $row['post_image'];
+        $post_tag = $row['post_tag'];
+        $post_comment_count = $row['post_comment_count'];
+        $post_date = $row['post_date'];
+        //$post_content = $row['post_content'];
+
+        echo "<tr>";
+        echo "<td>$post_id</td>";
+        echo "<td>$post_author</td>";
+        echo "<td>$post_title</td>";
+        echo "<td>$post_category_id</td>";
+        echo "<td>$post_status</td>";
+        echo "<td><img class='img-thumbnail' width=200 src='../images/$post_image' alt='post images'></td>";
+        echo "<td>$post_tag</td>";
+        echo "<td>$post_comment_count</td>";
+        echo "<td>$post_date</td>";
+        echo "</tr>";
+
+
+    }
+
+
+    ?>
+
+
+    <td>10</td>
+    <td>Gabor</td>
+    <td>Bootstrap framework</td>
+    <td>Bootstrap</td>
+    <td>Status</td>
+    <td>Image</td>
+    <td>Tags</td>
+    <td>Comments</td>
+    <td>Date</td>
+    </tbody>
+
+</table>
